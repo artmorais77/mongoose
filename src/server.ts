203382a741a5ed9486d8db7e853fe39f31ef4073
artmorais77 <arturmorais.ald@gmail.com>
2025-11-15 11:49:21 -0300

    chore(db): renomeia de database para config

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { app } from "./app"
 import { env } from "./env"
+import { connectDB } from "./config/db";
+
+connectDB();
 
 app.listen(env.PORT, () => console.log(`Servidor rodando na porta ${env.PORT}`))
